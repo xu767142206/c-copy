7383b68ec726811c4333d806b33dd0b45a2a1b73
vanve <767142206@qq.com>
2021-11-30 17:00:14 +0800

    first commit

---
+++ HZH_Controls/Test/FrmMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Test
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.ucSplitLine_V1 = new HZH_Controls.Controls.UCSplitLine_V();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.scrollbarComponent1 = new HZH_Controls.Controls.ScrollbarComponent(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ucSplitLine_V1
+            // 
+            this.ucSplitLine_V1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.ucSplitLine_V1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ucSplitLine_V1.Location = new System.Drawing.Point(0, 61);
+            this.ucSplitLine_V1.Name = "ucSplitLine_V1";
+            this.ucSplitLine_V1.Size = new System.Drawing.Size(1, 707);
+            this.ucSplitLine_V1.TabIndex = 8;
+            this.ucSplitLine_V1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(1, 730);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1353, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(619, 12);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(68, 17);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "顶顶顶顶顶";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 768);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ucSplitLine_V1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsFullSize = true;
+            this.IsShowCloseBtn = true;
+            this.Name = "FrmMain";
+            this.ShowIcon = true;
+            this.ShowInTaskbar = true;
+            this.Text = "FrmMain";
+            this.Title = "HZHControls控件库 DEMO";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.Controls.SetChildIndex(this.ucSplitLine_V1, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private HZH_Controls.Controls.UCSplitLine_V ucSplitLine_V1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private HZH_Controls.Controls.ScrollbarComponent scrollbarComponent1;
+    }
+}
